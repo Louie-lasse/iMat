cf692129baf97f9c8fd57511c2b918783b398997
Love <love.lindqvist101@gmail.com>
2021-05-11 15:31:50 +0200

    Försökte fixa saker, men inget gick rätt

--- src/resources/Page.java
+++ src/resources/Page.java
@@ -8,15 +8,7 @@ import se.chalmers.cse.dat216.project.IMatDataHandler;
 public abstract class Page extends AnchorPane {
     protected static IMatDataHandler db = IMatDataHandler.getInstance();
     protected static MenuController parent;
-    public void setParent(MenuController menuController){ parent = menuController; }
-    @FXML
-    protected void previousButtonPressed(Event e){
-        parent.showPreviousWindow();
-    }
-    @FXML
-    protected void nextButtonPressed(Event e){
-        parent.showNextWindow();
-    }
+    public static void setParent(MenuController menuController){ parent = menuController; }
     public abstract void update();
     protected abstract void initialize();
 }
