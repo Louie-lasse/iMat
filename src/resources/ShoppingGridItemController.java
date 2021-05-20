868745db8e86483d17a4387c96455adab01f357d
Mange99 <you@example.com>
2021-05-20 19:11:49 +0200

    grey subbtn if 0

--- src/resources/ShoppingGridItemController.java
+++ src/resources/ShoppingGridItemController.java
@@ -3,6 +3,7 @@ package resources;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
@@ -24,6 +25,9 @@ public class ShoppingGridItemController  extends AnchorPane {
     private Product product;
 
     private Unit unit;
+    @FXML Button subtract;
+    @FXML Button invalidSub;
+    @FXML Label subLabel;
 
     private static final BackendAdapter db = BackendAdapter.getInstance();
 
@@ -37,6 +41,15 @@ public class ShoppingGridItemController  extends AnchorPane {
             throw new RuntimeException(exception);
         }
 
+        amount.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(Integer.parseInt(onlyNumbers(amount.getText())) > 0){
+                subtract.toFront();
+            }else{
+                invalidSub.toFront();
+            }
+            subLabel.toFront();
+        });
+
 
         //Anropa init
 
@@ -45,6 +58,14 @@ public class ShoppingGridItemController  extends AnchorPane {
         initialize();
         update();
     }
+
+    private String onlyNumbers(String input){
+        if (!input.matches("\\d*")) {
+            return (input.replaceAll("[^\\d]", ""));
+        }
+        return input;
+    }
+
 
     private void initialize(){
         // Kan behövas för att synliggöra rundade hörn// scene.setFill(Color.TRANSPARENT); //Makes the back-background of the cards transparent
