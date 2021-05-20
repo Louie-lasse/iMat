7705f165210fe97031ed0ddda225b4e5a9bce769
Mange99 <you@example.com>
2021-05-20 12:13:17 +0200

    ksasaPage update zip coode

--- src/resources/KassaPage.java
+++ src/resources/KassaPage.java
@@ -61,11 +61,6 @@ public class KassaPage extends Page {
 
     @Override
     protected void initialize() {
-        name.setText(customer.getFirstName() + " " + customer.getLastName());
-        phoneNumber.setText(customer.getPhoneNumber());
-        email.setText(customer.getEmail());
-        address.setText(customer.getAddress() + ", " + customer.getPostCode() + ", " + customer.getPostAddress());
-
         toggleGroup = new ToggleGroup();
         card.setToggleGroup(toggleGroup);
         klarna.setToggleGroup(toggleGroup);
@@ -219,9 +214,29 @@ public class KassaPage extends Page {
                 break;
         }
     }
-
+    private String[] splitPostCode(String code){
+        StringBuilder zip = new StringBuilder();
+        StringBuilder city = new StringBuilder();
+        int i;
+        for (i = 0; i < code.length(); i++){
+            if (i == 5){
+                break;
+            }
+            if (Character.isDigit(code.charAt(i))){
+                zip.append(code.charAt(i));
+            } else {
+                break;
+            }
+        }
+        return new String[]{zip.toString(), city.append(code.substring(i)).toString()};
+    }
     @Override
     public void update() {
+        name.setText(customer.getFirstName() + " " + customer.getLastName());
+        phoneNumber.setText(customer.getPhoneNumber());
+        email.setText(customer.getEmail());
+        String[] postCode = splitPostCode(customer.getPostCode());
+        address.setText(customer.getAddress() + ", " + postCode[0] + ", " + postCode[1]);
 
     }
 
